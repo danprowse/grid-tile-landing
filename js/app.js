18b3271e51815b262e0e271ec75c9f154e9c3ce6
Dan Prowse <danprowse@hotmail.co.uk>
2020-10-28 17:46:34 +0000

    set/get name and reminder inputs moved to separate class

--- js/app.js
+++ js/app.js
@@ -1,63 +1,25 @@
+import ContentEditableManager from './managers/ContentEditableManager.js';
 //DOM Elements
 const nameInput = document.querySelector('#name-input'); 
 const reminderInput = document.querySelector('#reminder-input'); 
 
-//get name
-function getName() {
-  //check local
-  if(localStorage.getItem('name') === null) {
-      nameInput.textContent = '[Enter Name]'
-  } else {
-      nameInput.textContent = localStorage.getItem('name');
-  }
-}
+//manager classes
+const CEM = new ContentEditableManager(nameInput, reminderInput);
 
-// Set Name
 function setName(e) {
-  
-  if (e.type === 'keypress') {
-      //make sure keypress is enter
-      if (e.which == 13 || e.keyCode == 13) {
-          localStorage.setItem('name', e.target.innerText);
-          nameInput.blur();
-      }
-      
-  } else {
-      localStorage.setItem('name', e.target.innerText);
-  }
+    CEM.setValue('name', e);
 }
 
-function getFocus() {
-  //check local
-  if(localStorage.getItem('focus') === null) {
-      reminderInput.textContent = '[Enter Focus]'
-  } else {
-      reminderInput.textContent = localStorage.getItem('focus');
-  }
-}
-
-// Set Focus
-function setFocus(e) {
-  
-  if (e.type === 'keypress') {
-      //make sure keypress is enter
-      if (e.which == 13 || e.keyCode == 13) {
-          localStorage.setItem('focus', e.target.innerText);
-          reminderInput.blur();
-      }
-      
-  } else {
-      localStorage.setItem('focus', e.target.innerText);
-  }
+function setReminder(e) {
+    CEM.setValue('focus', e);
 }
 
 nameInput.addEventListener('keypress', setName);
 nameInput.addEventListener('blur', setName);
-reminderInput.addEventListener('keypress', setFocus);
-reminderInput.addEventListener('blur', setFocus);
-
+reminderInput.addEventListener('keypress', setReminder);
+reminderInput.addEventListener('blur', setReminder);
 
 
 //Run
-getName();
-getFocus();
+CEM.getValue('name');
+CEM.getValue('focus');
---
+++ js/managers/ContentEditableManager.js
@@ -0,0 +1,47 @@
+export default class ContentEditableManager {
+
+  #nameInputElement;
+  #reminderInputElement;
+  
+  constructor(name, reminder) {
+    this.#nameInputElement = name;
+    this.#reminderInputElement = reminder;
+  }
+
+  setValue(value, e) {    
+    if (e.type === 'keypress') {
+      //make sure keypress is enter
+      if (e.which == 13 || e.keyCode == 13) {
+          if(value === 'name') {
+            localStorage.setItem(value, this.#nameInputElement.innerText);
+            this.#nameInputElement.blur();
+          } else {
+            localStorage.setItem(value, this.#reminderInputElement.innerText);
+            this.#reminderInputElement.blur();
+          }
+      }
+    } else {
+      if(value === 'name') {
+        localStorage.setItem(value, this.#nameInputElement.innerText);
+      } else {
+        localStorage.setItem(value, this.#reminderInputElement.innerText);
+      }
+    }
+  }
+
+  getValue(value) {
+    if(localStorage.getItem(value) === null) {
+      if(value === 'name') {
+        this.#nameInputElement.textContent = `[Enter ${value}]`;
+      } else {
+        this.#reminderInputElement.textContent = `[Enter ${value}]`;
+      }
+    } else {
+        if(value === 'name') {
+          this.#nameInputElement.textContent = localStorage.getItem(value);;
+        } else {
+          this.#reminderInputElement.textContent = localStorage.getItem(value);;
+        }
+    }
+  }
+}
