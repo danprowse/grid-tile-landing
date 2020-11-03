0e3d2c99952e74584bb814516a4324f420258356
Dan Prowse <danprowse@hotmail.co.uk>
2020-11-03 19:00:08 +0000

    weather data inserted into DOM and diplaying icon img + temp + feels

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@ import ContentEditableManager from './managers/ContentEditableManager.js';
 import ApiManager from './managers/ApiManager.js';
 //DOM Elements
 const nameInput = document.querySelector('#name-input'); 
-const reminderInput = document.querySelector('#reminder-input'); 
+const reminderInput = document.querySelector('#reminder-input');
+const weatherIcon = document.querySelector('.icon'); 
+const currentTemp = document.querySelector('.current-temp'); 
+const feelLikeTemp = document.querySelector('.feels-like-temp'); 
 
 //manager classes
 const CEM = new ContentEditableManager(nameInput, reminderInput);
@@ -16,10 +19,22 @@ function setReminder(e) {
     CEM.setValue('focus', e);
 }
 
+async function getWeather() {
+    //get response
+    const weather = await AM.getWeather();
+    //add to DOM
+    weatherIcon.src = `${weather.icon}`;
+    currentTemp.innerHTML = `<p class="temp">${weather.temp_c}&#176;C</p>`
+    feelLikeTemp.innerHTML = `<p class="feels-temp">Feels like: ${weather.feels_like_c}&#176;C</p>`
+    // console.log(weather);
+}
+
 nameInput.addEventListener('keypress', setName);
 nameInput.addEventListener('blur', setName);
 reminderInput.addEventListener('keypress', setReminder);
 reminderInput.addEventListener('blur', setReminder);
+
+window.addEventListener('DOMContentLoaded', getWeather);
 
 
 //Run
--- js/managers/ApiManager.js
+++ js/managers/ApiManager.js
@@ -15,10 +15,22 @@ export default class ApiManager {
   }
 
   async getWeather() {
+    const weatherObj = {};
     const res = await fetch('http://localhost:5000/api/news-weather/weather')
-    .then(response => response.json());
-    console.log(res);
-    return res;
+    .then(response => response.json())
+    .then(data => {
+      weatherObj.temp_c = data.current.temp_c;
+      weatherObj.feels_like_c = data.current.feelslike_c;
+      weatherObj.icon = data.current.condition.icon;
+      console.log(weatherObj);
+    });
+    // const weatherObj = {
+    //   temp: res.current.temp_c,
+    //   feels_like: res.current.temp_c,
+    //   icon: res.current.condtion.icon
+    // }
+    // console.log(weatherObj);
+    return weatherObj;
   }
 
 }
