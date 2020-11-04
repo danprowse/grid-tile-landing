b99b8bd468d65c3a07c1a6edb57d39d42a29a1cb
Dan Prowse <danprowse@hotmail.co.uk>
2020-11-04 14:11:55 +0000

    test news data file added

---
+++ js/test-data/testData.js
@@ -0,0 +1,57 @@
+const novaraTestData = [
+  {
+    title: 'News Title',
+    permalink: '#',
+    thumb_small: 'https://images.pexels.com/photos/3866816/pexels-photo-3866816.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    title: 'News Title',
+    permalink: '#',
+    thumb_small: 'https://images.pexels.com/photos/3866816/pexels-photo-3866816.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    title: 'News Title',
+    permalink: '#',
+    thumb_small: 'https://images.pexels.com/photos/3866816/pexels-photo-3866816.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    title: 'News Title',
+    permalink: '#',
+    thumb_small: 'https://images.pexels.com/photos/3866816/pexels-photo-3866816.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    title: 'News Title',
+    permalink: '#',
+    thumb_small: 'https://images.pexels.com/photos/3866816/pexels-photo-3866816.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  }
+]
+
+const guardianTestData = [
+  {
+    sectionName: 'Politics',
+    webTitle: 'News Title',
+    webUrl: '#'
+  },
+  {
+    sectionName: 'US News',
+    webTitle: 'News Title',
+    webUrl: '#'
+  },
+  {
+    sectionName: 'UK Sport',
+    webTitle: 'News Title',
+    webUrl: '#'
+  },
+  {
+    sectionName: 'Politics',
+    webTitle: 'News Title',
+    webUrl: '#'
+  },
+  {
+    sectionName: 'Environment',
+    webTitle: 'Climate change is real',
+    webUrl: '#'
+  }
+]
+
+export { novaraTestData, guardianTestData };
