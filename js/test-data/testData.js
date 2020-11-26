dcd9bf2527f0ef18b13ef6d078a26c98ff5d58f9
Dan Prowse <danprowse@hotmail.co.uk>
2020-11-26 18:10:06 +0000

    test data for weather and news updated

--- js/test-data/testData.js
+++ js/test-data/testData.js
@@ -44,27 +44,27 @@ const novaraTestData = [
 const guardianTestData = [
   {
     sectionName: "Politics",
-    webTitle: "News Title",
-    webUrl: "#",
-  },
-  {
-    sectionName: "US News",
-    webTitle: "News Title",
-    webUrl: "#",
-  },
-  {
-    sectionName: "UK Sport",
-    webTitle: "News Title",
+    webTitle: "I’m a Discrimination Lawyer - the Labour Party Has Probably Broken the Law by Removing the Whip From Jeremy Corbyn",
     webUrl: "#",
   },
   {
     sectionName: "Politics",
-    webTitle: "News Title",
+    webTitle: "Sunak’s Spending Review Is a Drop of Water in a Desert - the Left Must Demand More",
     webUrl: "#",
   },
   {
-    sectionName: "Environment",
-    webTitle: "Climate change is real",
+    sectionName: "US News",
+    webTitle: "Who Will Represent the Young Now? As Biden takes record of Trump for being oldest elected President",
+    webUrl: "#",
+  },
+  {
+    sectionName: "US News",
+    webTitle: "Evil Without Villains: How Does the Left Challenge Empire After Trump?",
+    webUrl: "#",
+  },
+  {
+    sectionName: "World News",
+    webTitle: "The Rest of the World is Having a Trade Deal Party and Brexit Britain Isn’t Invited",
     webUrl: "#",
   },
 ];
@@ -73,7 +73,7 @@ const weatherTestData =
   {
     temp_c: "12.8",
     location: "London, UK",
-    icon: "#"
+    icon: "../img/weather-icon.png"
   };
 
 export { novaraTestData, guardianTestData, weatherTestData };
